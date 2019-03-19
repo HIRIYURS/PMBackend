d435af24d8c690aef49a8f627d61ba1dc19eb8b5
HIRIYURS <santhosh.shankar@gmail.com>
2019-03-19 22:00:16 +0530

    Completed all APIs

--- server.js
+++ server.js
@@ -59,7 +59,7 @@ router.route('/').get((req, res) => {
 
 // route/API for User table
 router.route('/users').get((req, res) => {
-    //logger.info('Request Received to Get Users!');
+    logger.info('Request Received to Get Users!');
     
     projmgr.User.find((err, users) => {
         if (err) {
@@ -126,7 +126,7 @@ router.route('/user/update/:id').post((req, res) => {
 });
 
 router.route('/user/delete/:id').get((req, res) => {
-    logger.info("Deleting Task: ", req.params.id);
+    logger.info("Deleting User: ", req.params.id);
     projmgr.User.findByIdAndRemove({_id: req.params.id}, (err, user) => {
         if (err) {
             res.json('Error deleting User');
@@ -138,24 +138,63 @@ router.route('/user/delete/:id').get((req, res) => {
 
 // router or API for Tasks
 router.route('/tasks').get((req, res) => {
-    logger.info('Request Received!');
-    res.status(200).json("Yet to implement. Thank you!");
+    //logger.info('Request Received to Get Tasks!');
+    
+    projmgr.Task.find((err, task) => {
+        if (err) {
+            logger.info('Error in Getting All Tasks');
+            res.status(400).send(err);
+        } else {
+            res.status(200).json(task);
+        }
+    });    
 
 });
 
 router.route('/tasks/:id').get((req, res) => {
-    res.status(200).json("Yet to implement. Thank you!");
+    //logger.info('Request Received to Get User By ID!');
+    projmgr.Task.findById(req.params.id, (err, task) => {
+        if (err) {
+            logger.info({ "message":"Error Getting the User",
+                          "Task ID": req.params.id,
+                          "Error": err
+                        });
+            res.status(400).json({});
+        } else {
+            res.json(task);
+        }
+    });
 });
 
 router.route('/gettasksbyproj/:id').get((req, res) => {
-    res.status(200).json("Yet to implement. Thank you!");
+    logger.info('Request Received to Get Tasks By Project!');
+    projmgr.User.find({ "parent": req.params.id}, (err, tasks) => {
+        if (err) {
+            logger.info({ "message":"Error Getting the Tasks for Project",
+                          "Project ID": req.params.id,
+                          "Error": err
+                        });
+            res.status(400).json({});
+        } else {
+            res.json(tasks);
+        }
+    });
 });
 
 router.route('/task/add').post((req, res) => {
     logger.info({ "message":"Adding New Task: ",
                   "req.body": req.body
                 });    
-    res.status(200).json("Yet to implement. Thank you!");
+    let task = new projmgr.Task(req.body);
+    task.save()
+        .then(task => {
+            res.status(200).json({'Task': 'Added Successfully'});
+            logger.info({ "message":"Adding Successfully"});
+        })
+        .catch(err => {
+            res.status(400).send('Failed to create new User');
+            logger.info({ "message":"Add Failed"});
+        });
 });
 
 router.route('/task/update/:id').post((req, res) => {
@@ -163,67 +202,188 @@ router.route('/task/update/:id').post((req, res) => {
                   "req.params.id": req.params.id,
                   "req.body": req.body
                 });
-    res.status(200).json("Yet to implement. Thank you!");
+    projmgr.Task.findById(req.params.id, (err, task) => {
+        if (!task)
+            return next(new Error('Could not load the User document'));
+        else {
+            task.task = req.body.task;
+            task.start_date = req.body.start_date;
+            task.end_date = req.body.end_date;
+            task.priority = req.body.priority;
+            task.status = req.body.status;
+            task.parent = req.body.parent;
+            task.user = req.body.user;
+            task.project = req.body.project;
+            task.save().then(task => {
+                res.status(200).json('Update Task Done');
+            }).catch(err => {
+                res.status(400).send('Update Task Failed!');
+            });
+        }
+    });
 });
 
 router.route('/task/delete/:id').get((req, res) => {
     logger.info("Deleting Task: ", req.params.id);
-    res.status(200).json("Yet to implement. Thank you!");
+    projmgr.Task.findByIdAndRemove({_id: req.params.id}, (err, task) => {
+        if (err) {
+            res.json('Error deleting Task');
+        } else {
+            res.json('Removed Task Successfully');
+        }
+    });
 });
 
 router.route('/task/endtask/:id').get((req, res) => {
     logger.info("Ending Task: ", req.params.id);
-    res.status(200).json("Yet to implement. Thank you!");
+    projmgr.Task.findById(req.params.id, (err, task) => {
+        if (!task)
+            return next(new Error('Could not load the document'));
+        else {
+            task.task = task.task;
+            task.start_date = task.start_date;
+            task.end_date = task.end_date;
+            task.priority = task.priority;
+            task.parent = task.parent;
+            task.status = "true";
+            task.save().then(task => {
+                res.json('Ended the task');
+            }).catch(err => {
+                res.status(400).send('End Task Failed!');
+            });
+        }
+    });
 });
 
 // route or API for Parent Task
 router.route('/parenttasks').get((req, res) => {
-    logger.info('Request Received!');
-    res.status(200).json("Yet to implement. Thank you!");
+    //logger.info('Request Received for getting all Parent Tasks!');
 
+    projmgr.Parent.find((err, ptasks) => {
+        if (err) {
+            res.status(400).send(err);
+        } else {
+            res.status(200).json(ptasks);
+        }
+    });
 });
 
 router.route('/parenttasks/:id').get((req, res) => {
-    res.status(200).json("Yet to implement get parenttask by id. Thank you!");
+    projmgr.Parent.findById(req.params.id, (err, ptask) => {
+        if (err) {
+            logger.info({ "message":"Error Getting the Parent Task",
+                          "Parent ID": req.params.id,
+                          "Error": err
+                        });
+            res.status(400).json({});
+        } else {
+            res.json(ptask);
+        }
+    });
 });
 
 router.route('/parenttask/add').post((req, res) => {
-    logger.info('Request Received! /parenttask/add');
-    logger.info({ "message":"Adding New Task: ",
+    logger.info('Request Received to add Parent Task!');
+    logger.info({ "message":"Adding New Parent Task: ",
                   "req.body": req.body
                 });    
-    res.status(200).json("Yet to implement add parenttask. Thank you!");
+    let ptask = new projmgr.Parent(req.body);
+    ptask.save()
+        .then(ptask => {
+            res.status(200).json({'Parent': 'Added Successfully'});
+            logger.info({ "message":"Adding Successfully"});
+        })
+        .catch(err => {
+            res.status(400).send('Failed to create new Parent Task');
+            logger.info({ "message":"Add Failed"});
+        });
+});
+
+router.route('/parenttask/delete/:id').get((req, res) => {
+    logger.info("Deleting Parent Task: ", req.params.id);
+    projmgr.Parent.findByIdAndRemove({_id: req.params.id}, (err, ptask) => {
+        if (err) {
+            res.json('Error deleting Parent Task');
+        } else {
+            res.json('Parent Task Removed Successfully');
+        }
+    });
 });
 
 // route or API for Projects
-router.route('/project').get((req, res) => {
-    logger.info('Request Received!');
-    res.status(200).json("Yet to implement. Thank you!");
-
+router.route('/projects').get((req, res) => {
+    logger.info('Request Received to add Project!');
+    projmgr.Project.find((err, projects) => {
+        if (err) {
+            res.status(400).send(err);
+        } else {
+            res.status(200).json(projects);
+        }
+    });
 });
 
-router.route('/project/:id').get((req, res) => {
-    res.status(200).json("Yet to implement. Thank you!");
+router.route('/projects/:id').get((req, res) => {
+    projmgr.Project.findById(req.params.id, (err, project) => {
+        if (err) {
+            logger.info({ "message":"Error Getting the project",
+                          "project ID": req.params.id,
+                          "Error": err
+                        });
+            res.status(400).json({});
+        } else {
+            res.json(project);
+        }
+    });
 });
 
 router.route('/project/add').post((req, res) => {
-    logger.info({ "message":"Adding New Task: ",
+    logger.info({ "message":"Adding New Project: ",
                   "req.body": req.body
                 });    
-    res.status(200).json("Yet to implement. Thank you!");
+    let project = new projmgr.Project(req.body);
+    project.save()
+        .then(project => {
+            res.status(200).json({'Project': 'Added Successfully'});
+            logger.info({ "message":"Project Added Successfully"});
+        })
+        .catch(err => {
+            res.status(400).send('Failed to create new Project');
+            logger.info({ "message":"project Add Failed"});
+        });
 });
 
 router.route('/project/update/:id').post((req, res) => {
-    logger.info({ "message":"Updating Task: ",
+    logger.info({ "message":"Updating Project: ",
                   "req.params.id": req.params.id,
                   "req.body": req.body
                 });
-    res.status(200).json("Yet to implement. Thank you!");
+    projmgr.Project.findById(req.params.id, (err, project) => {
+        if (!project)
+            return next(new Error('Could not load the Project document'));
+        else {
+            project.project_name = req.body.project_name;
+            project.start_date = req.body.start_date;
+            project.end_date = req.body.end_date;
+            project.priority = req.body.priority;
+            project.user = req.body.user;
+            project.save().then(project => {
+                res.status(200).json('Project Update Done');
+            }).catch(err => {
+                res.status(400).send('Project Update Failed!');
+            });
+        }
+    });
 });
 
 router.route('/project/delete/:id').get((req, res) => {
-    logger.info("Deleting Task: ", req.params.id);
-    res.status(200).json("Yet to implement. Thank you!");
+    logger.info("Deleting Project: ", req.params.id);
+    projmgr.Project.findByIdAndRemove({_id: req.params.id}, (err, project) => {
+        if (err) {
+            res.json('Error deleting Project');
+        } else {
+            res.json('Project Removed Successfully');
+        }
+    });
 });
 
 
